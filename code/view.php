9e5f3ea7c5df4bd852b79c1c772584542f4a982c
lukagrahor <41000453+lukagrahor@users.noreply.github.com>
2023-12-01 17:56:16 +0100

    Mesečni koledar is no more. Pozdravljen tedenski koledar
    ahja

--- code/view.php
+++ code/view.php
@@ -46,11 +46,12 @@ $_SESSION['expire_time'] = time() + $sess_expiration;
     <script src='https://cdn.jsdelivr.net/npm/fullcalendar@6.1.10/index.global.min.js'></script>
     <script>
     // to doda koledar iz full calendar API-ja
+    
       document.addEventListener('DOMContentLoaded', function() {
         var calendarEl = document.getElementById('calendar');
-        var calendar = new FullCalendar.Calendar(calendarEl, {
-          initialView: 'dayGridMonth'
-        });
+        const calendar = new FullCalendar.Calendar(calendarEl, {
+        initialView: 'timeGridWeek'
+        })
         calendar.render();
       });
     </script>
